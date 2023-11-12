34b44e517820f8ca8cbaa2821dc50ac4c0dc7db1
JrealFun <jeraldlee2002@gmail.com>
2023-11-12 13:30:10 +0800

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+class PalindromeChecker
+{
+
+    public static bool isPalindrome(string inputString, string trashSymbolsString)
+    {
+        if (string.IsNullOrEmpty(inputString) || inputString.Length == 1)
+            return false;
+
+        int start = 0;
+        int last = inputString.Length - 1;
+
+        while (start < last)
+        {
+            if (trashSymbolsString.Contains(inputString[start], StringComparison.OrdinalIgnoreCase))
+                start++;
+
+            if (trashSymbolsString.Contains(inputString[last], StringComparison.OrdinalIgnoreCase))
+                last--;
+
+            if (char.ToLower(inputString[start]) != char.ToLower(inputString[last]))
+                return false;
+
+            start++;
+            last--;
+        }
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        string inputString = "a@b!!b$a";
+        string trashSymbols = "!@$";
+
+        string inputString2 = "?Aa#c";
+        string trashSymbols2 = "#?";
+
+        Console.WriteLine(isPalindrome(inputString, trashSymbols));
+        Console.WriteLine(isPalindrome(inputString2, trashSymbols2));
+
+    }
+
+}
